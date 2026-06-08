6a7704f5896a4625fabfcc5f8ad69d6f9cc183db
zebamymoonamp <zebamymoonamp@gmail.com>
2026-06-08 06:28:17 +0530

    calculator made stateful

--- lib/calculator.dart
+++ lib/calculator.dart
@@ -1,7 +1,26 @@
 import 'package:flutter/material.dart';
 
-class Calculator extends StatelessWidget {
-  const Calculator({super.key});
+class Calculator extends StatefulWidget {
+  Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  TextEditingController heightc = TextEditingController();
+  String result = "";
+
+  TextEditingController weightc = TextEditingController();
+
+  void bmifinder() {
+    double height = double.parse(heightc.text) / 100;
+    double weight = double.parse(weightc.text);
+    double bmi = weight / (height * height);
+    setState(() {
+      result = "Your BMI is ${bmi.toStringAsFixed(2)}";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,34 +49,36 @@ class Calculator extends StatelessWidget {
             ),
             SizedBox(height: 100),
             SizedBox(
-              height: 300,
+              height: 350,
               width: 350,
               child: Card(
                 color: Colors.white,
                 child: Padding(
-                  padding: const EdgeInsets.only(left: 20,right: 20),
+                  padding: const EdgeInsets.only(left: 20, right: 20),
                   child: Column(
                     children: [
                       SizedBox(height: 20),
                       TextField(
+                        controller: heightc,
                         decoration: InputDecoration(
                           fillColor: Colors.grey,
                           filled: true,
-                          labelText: "Height",labelStyle: TextStyle(
-                            color:Colors.black,),
-                          //   
+                          labelText: "Height",
+                          labelStyle: TextStyle(color: Colors.black),
+                          //
                           //    hintText: "fgfhj",hintStyle: TextStyle(color: Colors.blue)
                           prefixIcon: Icon(Icons.height),
                           //suffixIcon: Icon(Icons.visibility)
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8),
-                            borderSide: BorderSide.none
+                            borderSide: BorderSide.none,
                           ),
                         ),
                       ),
-                  
+
                       SizedBox(height: 30),
                       TextField(
+                        controller: weightc,
                         decoration: InputDecoration(
                           fillColor: Colors.grey,
                           filled: true,
@@ -65,37 +86,63 @@ class Calculator extends StatelessWidget {
                           border: OutlineInputBorder(
                             borderRadius: BorderRadius.circular(8),
                             borderSide: BorderSide.none,
-                         
-                            ),   labelText:"Weight",labelStyle:TextStyle(
-                              color: Colors.black),
-                          )
+                          ),
+                          labelText: "Weight",
+                          labelStyle: TextStyle(color: Colors.black),
                         ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        Container(
+                      ),
+                      SizedBox(height: 30),
+                      GestureDetector(
+                        onTap: () {
+                          bmifinder();
+                        },
+                        child: Container(
                           height: 50,
                           width: 300,
-                          decoration:BoxDecoration(
-                            gradient: LinearGradient(colors: [
-                              Colors.orangeAccent,Colors.yellow,
-                            ]),
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [Colors.orangeAccent, Colors.yellow],
+                            ),
                             borderRadius: BorderRadius.circular(10),
-                            
-                          ) ,
-                          child:Center(
-                            child: Text("Calculate",
-                            style: 
-                            TextStyle(
-                              color: Colors.blue,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 20,
-                            
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Calculate",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
                             ),
-                                              
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+
+                      Container(
+                        color: Colors.transparent,
+                        child: Column(
+                          children: [
+                            Text(
+                              result,
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
                             ),
-                          ) ,
-                        )
+                            SizedBox(height:20),
+                            Text(
+                              "IDEAL BMI: Between 18.5 and 25.9",
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
---
+++ lib/sample.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Sampl extends StatelessWidget {
+  const Sampl({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold();
+  }
+}
